0e6347fa5a566375d8ab4a5d530478266bcbf330
Ardalan Samimi <ardalan.samimi@gmail.com>
2020-11-25 13:15:14 +0100

    Initial commit

--- Tests/NinjaControllerTests/NinjaControllerTests.swift
+++ Tests/NinjaControllerTests/NinjaControllerTests.swift
@@ -6,7 +6,7 @@ final class NinjaControllerTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(NinjaController().text, "Hello, World!")
+//        XCTAssertEqual(NinjaController().text, "Hello, World!")
     }
 
     static var allTests = [
